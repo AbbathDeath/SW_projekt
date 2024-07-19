fb0c37b4fba2d4813450e3b21bafd135bc417ab6
AbbathDeath <death.abbath@yandex.ru>
2024-07-20 00:57:06 +0200

    add controls

--- test1.py
+++ test1.py
@@ -4,10 +4,10 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-m1a = 32
-m1b = 31
-m2a = 36
-m2b = 35
+m1a = 31 #bialy32
+m1b = 32 #zolty31
+m2a = 35 #zielony36
+m2b = 36 #fielotowy35
 
 
 GPIO.setup(m1a, GPIO.OUT)
@@ -33,12 +33,28 @@ def motor_backward():
     GPIO.output(m2a, GPIO.LOW)
     GPIO.output(m2b, GPIO.HIGH)
 
+def motor_left():
+    GPIO.output(m1a, GPIO.HIGH)
+    GPIO.output(m1b, GPIO.LOW)
+    GPIO.output(m2a, GPIO.LOW)
+    GPIO.output(m2b, GPIO.HIGH)
+
+def motor_right():
+    GPIO.output(m1a, GPIO.LOW)
+    GPIO.output(m1b, GPIO.HIGH)
+    GPIO.output(m2a, GPIO.HIGH)
+    GPIO.output(m2b, GPIO.LOW)
+
 
 motor_forward()
 time.sleep(2)
-motor_stop()
-time.sleep(2)
 motor_backward()
 time.sleep(2)
+motor_left()
+time.sleep(2)
+motor_right()
+time.sleep(2)
 motor_stop()
+
+
 
